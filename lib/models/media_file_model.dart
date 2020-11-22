5067e9b3d52d47fcddb3f5d24f2e1dfa27d4290f
Neifen <neifen.b@gmailcom>
2020-11-22 02:50:42 +0100

    Made the way I get the downloadURL myself. Went from 1000ms per call to around 0

--- lib/models/media_file_model.dart
+++ lib/models/media_file_model.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/widgets.dart';
 
 class MediaFileModel {
+
   String fileName;
-  Uri url;
-  String contentType;
+  String url;
+  ContentType contentType;
   int dayNumber;
   Widget preSave;
 
-  MediaFileModel(this.dayNumber, this.fileName);
+  MediaFileModel(this.dayNumber, this.fileName){
+    if(this.fileName.endsWith("mp4")){
+      contentType = ContentType.video;
+    }
+    else if(this.fileName.endsWith("jpg") || this.fileName.endsWith("jpeg") || this.fileName.endsWith("png") || this.fileName.endsWith("gif")){
+      contentType = ContentType.image;
+    }
+    else{
+      contentType = ContentType.unknown;
+    }
+  }
 
+
+
+}
+
+enum ContentType{
+  video,
+  image,
+  unknown
 }
